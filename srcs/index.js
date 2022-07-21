2aa7474d66ce80c53f963bd83531bef56caab091
Axel Coézard <axel.coezard@gmail.com>
2022-07-21 14:33:47 +0200

    first commit

---
+++ srcs/index.js
@@ -0,0 +1,12 @@
+(async function () {
+
+	chrome.tabs.query({currentWindow: true, active: true}, async (tabs) => {
+		// On recupere la tab courante
+		const tab = tabs[0];
+		// On affiche si on est sur lintra ou non
+		if (!tab.url.includes("profile.intra.42.fr"))
+			return document.querySelector("#app").append("Pas sur l'intra");
+		document.querySelector("#app").append("Sur l'intra");
+	});
+
+})()
